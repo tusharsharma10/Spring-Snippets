2c370c92f99e800bdafe6b486f3111552c9f1e93
lenovo <sharmatushar759@gmail.com>
2020-05-08 01:21:24 +0530

    Added custom events

---
+++ src/main/java/com/snippets/events/CustomSpringEventListener.java
@@ -0,0 +1,16 @@
+package com.snippets.events;
+
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomSpringEventListener  {
+
+	@EventListener
+	public void onApplicationEvent(CustomSpringEvent event) {
+		System.out.println("Received spring custom event - " + event.getMessage());
+	}
+
+	
+	
+}
